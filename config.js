a895217f2e063b6200675514582038bbc6549062
eric-c-jackson <63906242+eric-c-jackson@users.noreply.github.com>
2020-04-18 15:35:34 +0100

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ var klaroConfig = {
     //cookieDomain: '.github.com',
 
     // Put a link to your privacy policy here (relative or absolute).
-    privacyPolicy: '/#privacy',
+    privacyPolicy: '/privacy',
 
     // Defines the default state for applications (true=enabled by default).
     default: false,
